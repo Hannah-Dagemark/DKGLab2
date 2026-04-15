4e33326cee97ee4a9e9df84610c017a26a3bfa8f
Hannah dagemark <dagemark@student.chalmers.se>
2026-04-15 15:08:20 +0200

    task 5 setup

---
+++ lab2/lib/widgets/recipe_list.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:lab2/model/recipe_database/recipe_handler.dart';
+import 'package:lab2/widgets/recipe_list_item.dart';
+import 'package:provider/provider.dart';
+
+class RecipeList extends StatelessWidget {
+  const RecipeList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var recipeHandler = context.watch<RecipeHandler>();
+    var recipes = recipeHandler.bestMatches;
+    return ListView(
+      children: [
+        for (final recipe in recipes)
+          Card(
+            elevation: 2,
+            margin: const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+            child: RecipeListItem(recipe, onTap: () {}),
+          ),
+      ],
+    );
+  }
+}
